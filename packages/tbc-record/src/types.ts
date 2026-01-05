ca0854a4a0dbece133d73eb24e57df2419ec63e4
Mahesh K Bhat <k.mahesh.bhat@gmail.com>
2026-01-05 12:53:58 +0530

    chore: minor cleanup

--- packages/tbc-record/src/types.ts
+++ packages/tbc-record/src/types.ts
@@ -28,13 +28,9 @@ type TBCRecordOperation = {
    IDs?: string[];
    collection?: string;
    records?: TBCRecord[];
-   // results?: TBCStore | Record<string, string[]>;
-   // accumulate?: TBCStore | Record<string, string[]>;
-   // empty?: TBCStore | Record<string, string[]>;
    results?: TBCStore;
    accumulate?: TBCStore;
    empty?: TBCStore;
-   // Query-specific fields
    query?: TBCQueryParams;
    queryResult?: TBCQueryResult;
 }
